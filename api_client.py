6061935baffbcd23a2c4d634487262431dca5044
Suxrob <suxrob@gmail.com>
2025-07-16 07:52:32 +0500

    Initial commit with inline query handler

--- api_client.py
+++ api_client.py
@@ -87,7 +87,8 @@ async def update_user(telegram_id, data):
         async with session.put(f"{BASE_URL}/users/{telegram_id}", json=data) as resp:
             return await resp.json()
 
-logger.warning(f"BUYURTMA JSON: {order_data}")
+print("BUYURTMA JSON:", order_data)
+
 
 async def create_order(data):
     async with aiohttp.ClientSession() as session:
